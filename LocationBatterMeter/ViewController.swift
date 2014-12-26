67a872dd785aa6541dd2a14cea94e54d5d8ed773
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-12-26 19:46:25 +1100

    Calculate location battery usage

--- LocationBatterMeter/ViewController.swift
+++ LocationBatterMeter/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+  let locationManager = CLLocationManager()
+  @IBOutlet weak var startTimeLabel: UILabel!
+  @IBOutlet weak var locationUpdateLabel: UILabel!
+  @IBOutlet weak var accuracyLabel: UILabel!
+  @IBOutlet weak var batteryLevelLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    locationManager.delegate = self
+    restartUpdatingLocation()
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func onStartTapped(sender: AnyObject) {
+    restartUpdatingLocation()
   }
 
+  private func restartUpdatingLocation() {
+    startTimeLabel.text = ""
+    locationUpdateLabel.text = ""
+    accuracyLabel.text = ""
 
+    locationManager.stopUpdatingLocation()
+
+    locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+    accuracyLabel.text = "Desired accuracy \(locationManager.desiredAccuracy)m"
+
+    startTimeLabel.text = "Started \(currentTime)"
+
+    updateBatteryLevel()
+
+    iiQ.runAfterDelay(1) {
+      self.startUpdatingLocation()
+    }
+  }
+
+  private func updateBatteryLevel() {
+    let device = UIDevice.currentDevice()
+    device.batteryMonitoringEnabled = true
+
+    batteryLevelLabel.text = "Started with \(Int(device.batteryLevel * 100))% battery"
+  }
+
+  private func startUpdatingLocation() {
+    if locationManager.respondsToSelector(Selector("requestAlwaysAuthorization")) {
+      locationManager.requestAlwaysAuthorization()
+    }
+
+    locationManager.startUpdatingLocation()
+  }
+
+  private var currentTime: String {
+    let date = NSDate()
+    let formatter = NSDateFormatter()
+    formatter.timeStyle = .MediumStyle
+    formatter.dateStyle = .ShortStyle
+    return formatter.stringFromDate(date)
+  }
 }
 
+// CLLocationManager Delegate
+// ------------------------------
+
+typealias ExtCLLocationManagerDelegate = ViewController
+
+extension ExtCLLocationManagerDelegate {
+  func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    if let currentLocation = locations.last as? CLLocation {
+      locationUpdateLabel.text =  "[\(currentTime)] accuracy: \(currentLocation.horizontalAccuracy)"
+    }
+  }
+}
